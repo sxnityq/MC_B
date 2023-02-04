487374fbe179d9f39bbd8eed158901e9f435f642
sxnity <markspet662@gmail.com>
2023-02-04 22:33:28 +0200

    minor changes in api route

--- MC_BS/main/urls.py
+++ MC_BS/main/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import Home, SoloNew, UserRegistrationBackend, GetAlbumApi, home
 
 urlpatterns = [
-    path('', view=Home.as_view()),
+    path('api/v1/', view=Home.as_view()),
     path('<slug:slug>/', view=SoloNew.as_view()),
     path('api/v1/registrate/', view=UserRegistrationBackend.as_view()),
     path('api/v1/album/<slug:slug>/', view=GetAlbumApi.as_view()),
