895908f0dd3905e39c6fd15febf7afd8c1511b13
sxnity <markspet662@gmail.com>
2023-03-12 20:06:30 +0200

    UPDATE - add yasg_swagger

--- MC_BS/main/urls.py
+++ MC_BS/main/urls.py
@@ -12,9 +12,9 @@ from .views import (Home, SoloNew, UserRegistrationBackend,
 urlpatterns = [
     path('api/v1/auth/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/registrate/', view=UserRegistrationBackend.as_view()),
     path('api/v1/news', view=Home.as_view()),
     path('api/v1/news/<slug:slug>/', view=SoloNew.as_view()),
-    path('api/v1/registrate/', view=UserRegistrationBackend.as_view()),
     path('api/v1/album/<slug:slug>/', view=GetAlbumApi.as_view()),
     path('api/v1/album/', view=AlbumListApi.as_view()),
     path('test', view=home)
